c7a828e6a090365a184e0a84f2c2313a21600e89
Jon Lee <39420293+4ka0@users.noreply.github.com>
2023-02-09 21:44:13 +0900

    Test update

--- kikai_compare/tests.py
+++ kikai_compare/tests.py
@@ -9,11 +9,24 @@ class TestInputForm(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.empty_form = InputForm()
+        cls.valid_form_ja_en = InputForm(
+            {
+                "direction": "Ja>En",
+                "source_text": "ゼルダの伝説シリーズは任天堂が開発しているコンピュータゲームシリーズ。",
+            }
+        )
+        cls.valid_form_en_ja = InputForm(
+            {
+                "direction": "En>Ja",
+                "source_text": "Zelda series is a computer game series developed by Nintendo.",
+            }
+        )
+
 
     # Test field properties
 
     def test_direction_label(self):
-        self.assertEqual(self.empty_form.fields["direction"].label, None)
+        self.assertFalse(self.empty_form.fields["direction"].label)
         self.assertNotEqual(self.empty_form.fields["direction"].label, "")
         self.assertNotEqual(self.empty_form.fields["direction"].label, "direction")
         self.assertNotEqual(self.empty_form.fields["direction"].label, "Direction")
@@ -37,7 +50,7 @@ class TestInputForm(TestCase):
         self.assertNotEqual(self.empty_form.fields['direction'].initial, "")
 
     def test_source_text_label(self):
-        self.assertEqual(self.empty_form.fields["source_text"].label, None)
+        self.assertFalse(self.empty_form.fields["source_text"].label, None)
         self.assertNotEqual(self.empty_form.fields["source_text"].label, "")
         self.assertNotEqual(self.empty_form.fields["source_text"].label, "test_source")
         self.assertNotEqual(self.empty_form.fields["source_text"].label, "Test source")
@@ -52,6 +65,32 @@ class TestInputForm(TestCase):
     def test_source_text_strip(self):
         self.assertTrue(self.empty_form.fields['source_text'].strip)
 
-    # Test submission of valid form
+    # Test submission of valid forms
+    def test_valid_form_ja_en(self):
+        self.assertTrue(self.valid_form_ja_en.is_bound)
+        self.assertTrue(self.valid_form_ja_en.is_valid())
+        self.assertEqual(len(self.valid_form_ja_en.errors), 0)
+        self.assertEqual(self.valid_form_ja_en.errors, {})
+        self.assertEqual(self.valid_form_ja_en.errors.as_text(), "")
+        self.assertEqual(self.valid_form_ja_en.cleaned_data["direction"], "Ja>En")
+        self.assertNotEqual(self.valid_form_ja_en.cleaned_data["direction"], "En>Ja")
+        self.assertEqual(
+            self.valid_form_ja_en.cleaned_data["source_text"],
+            "ゼルダの伝説シリーズは任天堂が開発しているコンピュータゲームシリーズ。"
+        )
+
+    def test_valid_form_en_ja(self):
+        self.assertTrue(self.valid_form_en_ja.is_bound)
+        self.assertTrue(self.valid_form_en_ja.is_valid())
+        self.assertEqual(len(self.valid_form_en_ja.errors), 0)
+        self.assertEqual(self.valid_form_en_ja.errors, {})
+        self.assertEqual(self.valid_form_en_ja.errors.as_text(), "")
+        self.assertEqual(self.valid_form_en_ja.cleaned_data["direction"], "En>Ja")
+        self.assertNotEqual(self.valid_form_en_ja.cleaned_data["direction"], "Ja>En")
+        self.assertEqual(
+            self.valid_form_en_ja.cleaned_data["source_text"],
+            "Zelda series is a computer game series developed by Nintendo."
+        )
+
     # Test submission of invalid form: empty form
     # Test submission of invalid form: invalid fields
