5f202bdfcbdbe8aea9308a698e7fcfbf3102bbd4
Jon Lee <39420293+4ka0@users.noreply.github.com>
2023-02-13 20:42:04 +0900

    Added deepl translation view

--- kikai_compare/views.py
+++ kikai_compare/views.py
@@ -1,17 +1,36 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.http import HttpResponseRedirect
-
 
 from .forms import InputForm
+
+from environs import Env
+import deepl
 
 
 def input_page_view(request):
     if request.method == 'POST':
         form = InputForm(request.POST)
         if form.is_valid():
-            # Process form data in form.cleaned_data as required
-            return HttpResponseRedirect(reverse_lazy('output_page'))
+
+            # Get form data
+            direction = form.cleaned_data["direction"]
+            source_text = form.cleaned_data["source_text"]
+
+            # Determine source and target languages
+            source_lang, target_lang = translation_direction(direction)
+
+            # Get translation results
+            deepl_result = call_deepl_api(source_text, source_lang, target_lang)
+
+            return render(
+                request,
+                "output.html",
+                {
+                    "source_text": source_text,
+                    "source_lang": source_lang,
+                    "target_lang": target_lang,
+                    "deepl_result": deepl_result,
+                },
+            )
     else:
         form = InputForm()
     return render(request, 'input.html', {'form': form})
@@ -19,3 +38,24 @@ def input_page_view(request):
 
 def output_page_view(request):
     return render(request, 'output.html')
+
+
+def translation_direction(direction):
+    if direction == "Ja>En":
+        return "ja", "en-us"
+    else:
+        return "en-us", "ja"
+
+
+def call_deepl_api(source_text, source_lang, target_lang):
+    env = Env()
+    env.read_env()
+    auth_key = env.str("DEEPL_AUTH_KEY")
+    translator = deepl.Translator(auth_key)
+    deepl_result = translator.translate_text(
+                source_text,
+                source_lang=source_lang,
+                target_lang=target_lang,
+                glossary=None,
+            )
+    return deepl_result
