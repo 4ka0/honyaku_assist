22d356e9ad9cbf68d31a6f7f771882960a902178
Jon Lee <39420293+4ka0@users.noreply.github.com>
2023-02-14 17:17:23 +0900

    Urls simplified

--- kikai_compare/urls.py
+++ kikai_compare/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from .views import input_page_view, output_page_view
+from .views import input_page_view
 
 
 urlpatterns = [
-    path('input', input_page_view, name='input_page'),
-    path('output', output_page_view, name='output_page'),
+    path('', input_page_view, name='input_page'),
 ]
