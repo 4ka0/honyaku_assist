79f560e29d0c0965bf46af43c8afcd6aff37a88d
Jon Lee <39420293+4ka0@users.noreply.github.com>
2023-02-19 20:26:36 +0900

    Included string lengths in output

--- kikai_compare/views.py
+++ kikai_compare/views.py
@@ -17,25 +17,32 @@ def input_page_view(request):
             # Get form data
             direction = form.cleaned_data["direction"]
             source_text = form.cleaned_data["source_text"]
+            source_text_length = len(source_text)
 
             # Determine source and target languages
             source_lang, target_lang = translation_direction(direction)
 
             # Get translation results
             deepl_result, deepl_usage = call_deepl_api(source_text, source_lang, target_lang)
+            deepl_result_length = len(str(deepl_result))
+            
             google_result = call_google_api(source_text, source_lang, target_lang)
+            google_result_length = len(google_result)
 
             return render(
                 request,
                 "output.html",
                 {
                     "source_text": source_text,
+                    "source_text_length": source_text_length,
                     "source_lang": source_lang,
                     "target_lang": target_lang,
                     "deepl_result": deepl_result,
+                    "deepl_result_length": deepl_result_length,
                     "deepl_usage": deepl_usage.character.count,
                     "deepl_limit": deepl_usage.character.limit,
                     "google_result": google_result,
+                    "google_result_length": google_result_length,
                 },
             )
     else:
