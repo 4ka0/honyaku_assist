8597f7ea7791527aa505409c7ae5061bbaf3961c
Jon Lee <39420293+4ka0@users.noreply.github.com>
2023-03-17 13:06:41 +0900

    Tests split into separate files

---
+++ honyaku_assist/tests/test_utils.py
@@ -0,0 +1,108 @@
+from unittest.mock import patch
+
+from django.urls import reverse
+from django.test import SimpleTestCase
+
+import deepl
+
+
+class TestOutput(SimpleTestCase):
+
+    def setUp(self):
+
+        # The below makes actual calls to the DeepL and Google APIs.
+        """
+        self.valid_data_response = self.client.post(
+            reverse('translate'),
+            {'direction': 'Ja>En', 'source_text': '花粉飛散情報'}
+        )
+        """
+
+        # The below mocks the calls to the DeepL and Google APIs.
+        with patch('honyaku_assist.views.call_deepl_api') as mock_deepl_call, \
+            patch('honyaku_assist.views.call_google_api_v3') as mock_google_call:
+
+            # Mock the values returned by call_deepl_api() in the view.
+            deepl_result, deepl_usage = 'Pollen dispersal information', 6
+            mock_deepl_call.return_value = deepl_result, deepl_usage
+
+            # Mock the values returned by call_google_api_v3() in the view.
+            google_result = 'Pollen dispersion information'
+            mock_google_call.return_value = google_result
+
+            # Make a request to the view.
+            self.response = self.client.post(
+                reverse('translate'),
+                {'direction': 'Ja>En', 'source_text': '花粉飛散情報'}
+            )
+
+            # Assert that mocked functions are actually called.
+            mock_deepl_call.assert_called_once()
+            mock_google_call.assert_called_once()
+
+    def test_output_page_response_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_output_page_context_contains_submitted_data(self):
+        self.assertEqual(self.response.context['source_lang'], 'ja')
+        self.assertEqual(self.response.context['target_lang'], 'en-us')
+        self.assertEqual(self.response.context['source_text'], '花粉飛散情報')
+        self.assertEqual(self.response.context['source_text_length'], 6)
+
+    def test_output_page_context_deepl_data(self):
+        self.assertEqual(self.response.context['deepl_result'], 'Pollen dispersal information')
+        self.assertEqual(self.response.context['deepl_result_length'], 28)
+        self.assertEqual(self.response.context['deepl_usage'], 6)
+
+    def test_output_page_context_google_data(self):
+        self.assertEqual(self.response.context['google_result'], 'Pollen dispersion information')
+        self.assertEqual(self.response.context['google_result_length'], 29)
+
+    def test_output_page_templates_used(self):
+        self.assertTemplateUsed(self.response, 'base.html')
+        self.assertTemplateUsed(self.response, 'output.html')
+
+    def test_output_page_content(self):
+        self.assertContains(self.response, 'Source text (JA)')
+        self.assertContains(self.response, '(No. of characters: 6)')
+        self.assertContains(self.response, 'DeepL result (EN-US)')
+        self.assertContains(self.response, 'Pollen dispersal information')
+        self.assertContains(self.response, 'Google result (EN-US)')
+        self.assertContains(self.response, 'Pollen dispersion information')
+
+
+class TestDeepLOutputErrors(SimpleTestCase):
+
+    def test_deepl_api_exception_handling_1(self):
+
+        with patch.object(deepl.Translator, 'translate_text') as mock_deepl_translator:
+
+            # Mock the value returned by translate_text()
+            mock_deepl_translator.return_value = 'A DeepL translation error occurred.'
+
+            # Make a request to the view.
+            self.response = self.client.post(
+                reverse('translate'),
+                {'direction': 'Ja>En', 'source_text': '花粉飛散情報'}
+            )
+
+            # Assert that the mocked function is actually called.
+            mock_deepl_translator.assert_called_once()
+
+    @patch('deepl.Translator')
+    def test_deepl_api_exception_handling_1(self, mock_deepl_translator):
+        mock_deepl_translator.translate_text.return_value = 'A DeepL translation error occurred.'
+        mock_deepl_translator.get_usage.return_value = 10
+
+        # Make a request to the view.
+        response = self.client.post(
+            reverse('translate'),
+            {'direction': 'Ja>En', 'source_text': '花粉飛散情報'}
+        )
+
+        mock_deepl_translator.assert_called_once()
+
+        self.assertEqual(response.status_code, 200)
+
+        print(response.content)
+        print(response.context)
--- honyaku_assist/tests/test_view.py
+++ honyaku_assist/tests/test_view.py
@@ -4,114 +4,38 @@ from django.urls import reverse
 from django.test import SimpleTestCase
 
 
-class TestHomepage(SimpleTestCase):
+class TestTranslateView(SimpleTestCase):
 
-    def test_home_page_exists_at_desired_url(self):
+    def test_page_exists_at_desired_url(self):
         response = self.client.get('')
         self.assertEqual(response.status_code, 200)
 
-    def test_home_page_accessible_by_name(self):
-        response = self.client.get(reverse('home'))
+    def test_page_accessible_by_name(self):
+        response = self.client.get(reverse('translate'))
         self.assertEqual(response.status_code, 200)
 
-    def test_home_page_uses_correct_template(self):
-        response = self.client.get(reverse('home'))
+    def test_page_uses_correct_template(self):
+        response = self.client.get(reverse('translate'))
         self.assertTemplateUsed(response, 'base.html')
         self.assertTemplateUsed(response, 'input.html')
 
-    def test_home_page_title_and_navbar_content(self):
-        response = self.client.get(reverse('home'))
+    def test_page_title_and_navbar_content(self):
+        response = self.client.get(reverse('translate'))
         self.assertContains(response, '<title>Honyaku Assist</title>')
 
-    def test_home_page_navbar_content(self):
-        response = self.client.get(reverse('home'))
+    def test_page_navbar_content(self):
+        response = self.client.get(reverse('translate'))
         self.assertContains(response, '<a class="navbar-brand" href="/">Honyaku Assist</a>')
         self.assertContains(
             response,
             '<span class="navbar-text">A machine translation assistant for'
         )
 
-    def test_home_page_form_content(self):
-        response = self.client.get(reverse('home'))
+    def test_page_form_content(self):
+        response = self.client.get(reverse('translate'))
         self.assertContains(response, 'Japanese to English')
         self.assertContains(response, 'English to Japanese')
         self.assertContains(
             response,
             '<button type="submit" class="btn btn-primary mt-2">Translate</button>'
         )
-
-
-class TestOutput(SimpleTestCase):
-
-    def setUp(self):
-
-        # The below makes actual calls to the DeepL and Google APIs.
-        """
-        self.valid_data_response = self.client.post(
-            reverse('home'),
-            {'direction': 'Ja>En', 'source_text': '花粉飛散情報'}
-        )
-        """
-
-        # The below mocks the calls to the DeepL and Google APIs.
-        with patch('honyaku_assist.views.call_deepl_api') as mock_deepl_call, \
-            patch('honyaku_assist.views.call_google_api_v3') as mock_google_call:
-
-            # Mock the values returned by call_deepl_api() in the view.
-            deepl_result, deepl_usage = 'Pollen dispersal information', 6
-            mock_deepl_call.return_value = deepl_result, deepl_usage
-
-            # Mock the values returned by call_google_api_v3() in the view.
-            google_result = 'Pollen dispersion information'
-            mock_google_call.return_value = google_result
-
-            # Make a request to the view.
-            self.response = self.client.post(
-                reverse('home'),
-                {'direction': 'Ja>En', 'source_text': '花粉飛散情報'}
-            )
-
-            # Assert that mocked functions are actually called.
-            mock_deepl_call.assert_called_once()
-            mock_google_call.assert_called_once()
-
-    def test_output_page_response_status_code(self):
-        self.assertEqual(self.response.status_code, 200)
-
-    def test_output_page_context_contains_submitted_data(self):
-        self.assertEqual(self.response.context['source_lang'], 'ja')
-        self.assertEqual(self.response.context['target_lang'], 'en-us')
-        self.assertEqual(self.response.context['source_text'], '花粉飛散情報')
-        self.assertEqual(self.response.context['source_text_length'], 6)
-
-    def test_output_page_context_deepl_data(self):
-        self.assertEqual(self.response.context['deepl_result'], 'Pollen dispersal information')
-        self.assertEqual(self.response.context['deepl_result_length'], 28)
-        self.assertEqual(self.response.context['deepl_usage'], 6)
-
-    def test_output_page_context_google_data(self):
-        self.assertEqual(self.response.context['google_result'], 'Pollen dispersion information')
-        self.assertEqual(self.response.context['google_result_length'], 29)
-
-    def test_output_page_templates_used(self):
-        self.assertTemplateUsed(self.response, 'base.html')
-        self.assertTemplateUsed(self.response, 'output.html')
-
-    def test_output_page_content(self):
-        self.assertContains(self.response, 'Source text (JA)')
-        self.assertContains(self.response, '(No. of characters: 6)')
-        self.assertContains(self.response, 'DeepL result (EN-US)')
-        self.assertContains(self.response, 'Pollen dispersal information')
-        self.assertContains(self.response, 'Google result (EN-US)')
-        self.assertContains(self.response, 'Pollen dispersion information')
-
-    def test_deep_api_exception_handling(self):
-        # When exception is raised, test that error message included in output.html.
-        # Need to do this with real (unmocked) API calls.
-        # Example, when en-us is set as the source language code:
-        # (DeepL Error: Bad request, message: Value for 'source_lang' not supported.)
-        pass
-
-    def test_google_api_exception_handling(self):
-        # Same as above, except for the Google API.
-        pass
